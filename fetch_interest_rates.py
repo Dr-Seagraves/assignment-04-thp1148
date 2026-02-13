c7181804256a8ee90544628e39da0bd1cc1462a8
thp1148 <thp1148@utulsa.edu>
2026-02-13 15:13:43 +0000

    add FRED API key for fetching interest rates

--- fetch_interest_rates.py
+++ fetch_interest_rates.py
@@ -18,7 +18,7 @@ import requests
 
 # FRED API key (free at https://fred.stlouisfed.org/docs/api/api_key.html)
 # Option 1: Put your key here (works for you; don't commit if sharing code)
-FRED_API_KEY = ""
+FRED_API_KEY = "824bda67e72716994282c477f1c988d7"
 
 # Option 2: If FRED_API_KEY above is empty, the script will use the FRED_API_KEY
 #            environment variable (set in terminal or .env file).
